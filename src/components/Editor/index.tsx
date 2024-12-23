bf8514740af8c7b3fdd46775a94efc0846a96c14
yewonJin <jyw966@naver.com>
2024-12-23 19:20:52 +0900

    design: 에디터 width 상대적크기로 변경

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,22 +1,29 @@
 'use client';
 
-import { useEditor, EditorContent } from '@tiptap/react';
+import { useEditor, EditorContent, Editor as EditorType } from '@tiptap/react';
 
 import { extensions } from './extensions';
 import '@/shared/highlight-theme.css';
 import '@/shared/typography.css';
 
-// TODO: 링크 + 이미지
-export default function Editor() {
+type EditorProps = {
+  onEditorUpdate: ({ editor }: { editor: EditorType }) => void;
+};
+
+// TODO: 이미지
+export default function Editor({
+  onEditorUpdate: handleEditorUpdate,
+}: EditorProps) {
   const editor = useEditor({
     extensions,
     content: '<p>Hello World! 🌎️</p>',
     immediatelyRender: false,
     editorProps: {
       attributes: {
-        class: 'prose prose-lg m-5 focus:outline-none',
+        class: 'prose prose-lg focus:outline-none !max-w-none',
       },
     },
+    onUpdate: handleEditorUpdate,
   });
 
   return <EditorContent editor={editor} />;
