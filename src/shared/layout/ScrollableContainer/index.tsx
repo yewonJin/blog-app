ffb882a8643cb1bcae6f24de5ca1751abd5e7117
yewonJin <jyw966@naver.com>
2024-12-23 17:10:48 +0900

    feat: 스크롤 컨테이너 구현

---
+++ src/shared/layout/ScrollableContainer/index.tsx
@@ -0,0 +1,21 @@
+import { Tailwindest } from 'tailwindest';
+
+import { cn } from '@/shared/utils';
+
+type Props = {
+  children: React.ReactNode;
+  height?: Tailwindest['height'];
+  maxHeight?: Tailwindest['maxHeight'];
+};
+
+export default function ScrollableContainer({
+  children,
+  height,
+  maxHeight,
+}: Props) {
+  return (
+    <div className={cn(height, maxHeight, 'scrollbar overflow-y-auto')}>
+      {children}
+    </div>
+  );
+}
